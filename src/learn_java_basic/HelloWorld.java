df9b3e80c650459ad52429a5d7f59d975a0343bc
quangdm1702 <doanminhquang17022003@gmail.com>
2024-09-18 23:43:24 +0700

    math function

--- src/learn_java_basic/HelloWorld.java
+++ src/learn_java_basic/HelloWorld.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Your name is: ");
-		String name = scanner.nextLine();
-		System.out.println("Your age is: ");
-		int age = scanner.nextInt();
-		System.out.println("My name is: " + name + ", age = " + age);
+		System.out.println("First number: ");
+		int firstNumber = scanner.nextInt();
+		System.out.println("Second number: ");
+		int secondNumber = scanner.nextInt();
+		System.out.println("Min = " + Math.min(firstNumber, secondNumber));
 		scanner.close();
 	}
 }
